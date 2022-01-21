25c5282ddfb788b953f46712617ec7c72372cb78
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-01-21 16:53:26 +0900

    [스프링 입문] 회원 관리 예제 - 웹 MVC 개발

--- 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 // 스프링 컨테이너가 처음에 스프링을 뜰 때, @Controller 어노테이션으로 인해 MemberController 객체를 생성해서 스프링에 넣어두고 관리 됨
@@ -20,5 +26,29 @@ public class MemberController {
     // 객체 의존관계를 외부에서 넣어주는 것을 DI (Dependency Injection), 의존성 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm"; // 단지 createMemberForm.html이 뿌려짐
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName()); // form에서 getName으로 name을 꺼냄
+
+        // System.out.println("member = " +member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 회원 가입 후 홈 화면으로
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
