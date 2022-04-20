e58c3e1a8f3a5c4e81872fb1422543b620c0c8c6
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-04-20 12:24:32 +0900

    [자바 ORM 표준 JPA 프로그래밍] 값 타입 - 실전 예제

---
+++ 자바 ORM 표준 JPA 프로그래밍/ch09-model6/src/main/java/jpabook/model/entity/Member.java
@@ -0,0 +1,67 @@
+package jpabook.model.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    // private String city; // 삭제
+    // private String street; // 삭제
+    // private String zipcode; // 삭제
+
+    @Embedded // 추가
+    private Address address; // 추가
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<Order>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
