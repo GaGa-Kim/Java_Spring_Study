9aa29575154b0756bb37eba0f7d029a412153edc
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-08-02 16:29:26 +0900

    [자바 스프링부트 활용 웹개발 실무용] Mybatis foreach 배열 검색 조건 추가

--- 자바 스프링부트 활용 웹개발 실무용/example-spring/src/main/java/com/example/examplespring/mvc/parameter/BoardSearchParameter.java
+++ 자바 스프링부트 활용 웹개발 실무용/example-spring/src/main/java/com/example/examplespring/mvc/parameter/BoardSearchParameter.java
@@ -2,6 +2,8 @@ package com.example.examplespring.mvc.parameter;
 
 import com.example.examplespring.mvc.domain.BoardType;
 
+import java.util.List;
+
 /**
  * 게시물 검색 파라미터
  * @author gagyeong
@@ -9,6 +11,7 @@ import com.example.examplespring.mvc.domain.BoardType;
 public class BoardSearchParameter {
 
     private String keyword;
+    private List<BoardType> boardTypes;
 
     public BoardSearchParameter() {
 
