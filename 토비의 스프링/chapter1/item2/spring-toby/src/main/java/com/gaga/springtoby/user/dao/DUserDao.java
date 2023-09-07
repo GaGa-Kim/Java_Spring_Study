920bb65ecef0d05456c7c3f0ed9d15a7b4d32f63
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-09-07 21:40:00 +0900

    [토비의 스프링 3.1] 1장. 오브젝트와 의존관계 - 1.2) DAO의 분리

--- 토비의 스프링/chapter1/item2/spring-toby/src/main/java/com/gaga/springtoby/user/dao/DUserDao.java
+++ 토비의 스프링/chapter1/item2/spring-toby/src/main/java/com/gaga/springtoby/user/dao/DUserDao.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DUserDao extends UserDao {
-    public Connection getConnection() throws ClassNotFoundException, SQLException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         // D사 DB connection 생성코드
         Class.forName("com.mysql.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby", "toby", "gaga");
--- 토비의 스프링/chapter1/item2/spring-toby/src/main/java/com/gaga/springtoby/user/dao/NUserDao.java
+++ 토비의 스프링/chapter1/item2/spring-toby/src/main/java/com/gaga/springtoby/user/dao/NUserDao.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class NUserDao extends UserDao {
-    public Connection getConnection() throws ClassNotFoundException, SQLException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         // N사 DB connection 생성코드
         Class.forName("com.mysql.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby", "toby", "gaga");
