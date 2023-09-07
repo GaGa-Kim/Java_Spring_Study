41a96aa0c87926c9a0a52ced6e326e8c1aedeb77
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-09-07 22:12:31 +0900

    [토비의 스프링 3.1] 1장. 오브젝트와 의존관계 - 1.3) DAO의 확장

---
+++ 토비의 스프링/chapter1/item3/spring-toby/src/main/java/com/gaga/springtoby/user/dao/ConnectionMaker.java
@@ -0,0 +1,11 @@
+package com.gaga.springtoby.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * ConnectionMaker 인터페이스
+ */
+public interface ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException;
+}
