e1fb90ee2027dab3ec20b881544fe0d3890b8119
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-09-07 22:17:19 +0900

    [토비의 스프링 Vol.1] 1장. 오브젝트와 의존관계 - 1.2) DAO의 분리

---
+++ 토비의 스프링 Vol.1/item2/spring-toby/src/main/java/com/gaga/springtoby/user/dao/DUserDao.java
@@ -0,0 +1,14 @@
+package com.gaga.springtoby.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        // D사 DB connection 생성코드
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby", "toby", "gaga");
+        return c;
+    }
+}
