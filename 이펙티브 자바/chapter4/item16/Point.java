5edc5e5f9d906b7fc890fbe15e6407c0268aff0e
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-10-02 18:37:10 +0900

    [이펙티브 자바] 아이템 16. public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라.

---
+++ 이펙티브 자바/chapter4/item16/Point.java
@@ -0,0 +1,28 @@
+package chapter4.item16;
+
+// 접근자와 변경자 메서드를 활용해 데이터를 캡슐화한다.
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
